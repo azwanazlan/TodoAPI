79ecc3a864432eea2b16d43d1c6527a2e6bdea80
Azwan Azlan <azwanazlan2809@gmail.com>
2023-02-12 12:53:58 +0800

    Added todoItemController

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -12,3 +12,10 @@ public class TodoContext : DbContext
 
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
 }
+
+/* 
+ * The database context is the main class that coordinates Entity Framework 
+ * functionality for a data model. This class is created by deriving from the 
+ * Microsoft.EntityFrameworkCore.DbContext class.
+ * 
+ */
